f19fbe1d4a6c52c1eebba2bf54f7a47a36ac79fc
Subham Singh <geekysubham@gmail.com>
2021-06-16 02:19:43 +0530

    add more infos in setup

--- saveit/setup.py
+++ saveit/setup.py
@@ -3,15 +3,18 @@ from setuptools import setup
 setup(
     name="saveit",
     verson="1.0",
-    py_modules=['saveit'],
-    install_requires=[
-        'Click',
-    ],
+    author="Subham Singh",
+    author_email="imsks007@gmail.com",
+    description="A cli utility to write text ina a file",
+    py_modules=["saveit"],
+    url="https://github.com/imsubhamsingh/saveit",
+    issues="https://github.com/imsubhamsingh/saveit/issues",
+    license="MIT License",
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=["Click"],
     entry_points="""
         [console_scripts]
         saveit=saveit:cli
     """,
-
-
-
 )
