607c87493eb8c5d160c657f64b494eb55dac23d5
Subham Singh <geekysubham@gmail.com>
2021-06-16 01:59:55 +0530

    remove unused code

--- saveit/saveit.py
+++ saveit/saveit.py
@@ -1,20 +1,17 @@
 import click
 
-@click.command()
-@click.option('--string', default='',
-            help="Enter anything in quotes.")
-@click.argument('filename', type=click.File('w'), default='-',
-            required=False)
 
+@click.command()
+@click.option("--string", default="", help="Enter anything in quotes.")
+@click.argument("filename", type=click.File("w"), default="-", required=False)
 def cli(string, filename):
     """
     Creates a text file and save your data there.
     """
-    # click.echo(out)
     data = filename.write(string)
     click.echo(file=filename)
-    return click.echo("Your data is saved successfully.") if data else ''
+    return click.echo("Your data is saved successfully.") if data else ""
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
